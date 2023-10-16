2ff4e08cc7c5901cfc87beda7b83b0248384ba3c
Gustavo Gonzaga <gustavosg91@gmail.com>
2023-10-16 13:16:34 -0300

    Jogos da palavras

---
+++ src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Laranja", "Maçã", "Uva", "Morango"],
+    corpo: ["Braço", "Perna", "Pescoço", "Orelha", "Olho", "Boca"],
+    computador: ["Lingaugem", "Framework", "Javascript", "React"],
+    alimento: ["Leite", "Arroz", "Feijão", "Carne", "Macarrão"],
+};
